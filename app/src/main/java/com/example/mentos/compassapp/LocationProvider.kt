81f59aa99c68a30e75662b16b3898e5e52437665
Rafał Wojnecki <rafal.wojnecki@gmail.com>
2019-04-20 03:22:33 +0200

    Add location provider

---
+++ app/src/main/java/com/example/mentos/compassapp/LocationProvider.kt
@@ -0,0 +1,22 @@
+package com.example.mentos.compassapp
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.location.Location
+import com.google.android.gms.location.LocationRequest
+import com.patloew.rxlocation.RxLocation
+import io.reactivex.Observable
+
+class LocationProvider(appContext: Context) {
+
+    private val rxLocation = RxLocation(appContext)
+
+    private val locationRequest = LocationRequest.create()
+        .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+        .setInterval(5000)
+
+    @SuppressLint("MissingPermission")
+    val currentLocation: Observable<Location> = rxLocation.location()
+        .updates(locationRequest)
+        .onExceptionResumeNext(Observable.empty()) // without permissions, just return empty
+}
--- app/src/main/java/com/example/mentos/compassapp/MainActivity.kt
+++ app/src/main/java/com/example/mentos/compassapp/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.example.mentos.compassapp
 
+import android.location.Location
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import io.reactivex.disposables.CompositeDisposable
 import kotlinx.android.synthetic.main.activity_main.*
+import com.patloew.rxlocation.RxLocation
+import com.google.android.gms.location.LocationRequest
+import io.reactivex.Observable
+
 
 class MainActivity : AppCompatActivity() {
 
     private val disposables by lazy { CompositeDisposable() }
 
     private val azimuthProvider: AzimuthProvider by lazy { AzimuthProvider(this) }
+    private val locationProvider: LocationProvider by lazy { LocationProvider(this) }
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +29,11 @@ class MainActivity : AppCompatActivity() {
         azimuthProvider.azimuthInDegrees
             .subscribe(::showAzimuth)
             .apply { disposables.add(this) }
+
+
+        locationProvider.currentLocation
+            .subscribe(::showLocation)
+            .apply { disposables.add(this) }
     }
 
     override fun onStop() {
@@ -33,6 +44,11 @@ class MainActivity : AppCompatActivity() {
     private fun showAzimuth(azimuth: Int) {
         Log.d("LOL", "azimuth $azimuth")
         textView.text = (azimuth).toString()
+    }
+
+    private fun showLocation(location: Location) {
+        Log.d("LOL", "location $location")
+        tvLocation.text = "lat ${location.latitude} lng ${location.longitude}"
     }
 }
 
